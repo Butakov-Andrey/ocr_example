c532f2ab77987255a745607ac10166da2f1daba4
Andrey Butakov <6669.butakov@gmail.com>
2024-12-05 17:37:37 +0300

    Add easyocr

--- app/main.py
+++ app/main.py
@@ -3,13 +3,17 @@ from ocr_tesseract import tesseract_extract
 
 
 def main():
-    image_path = "example_images/example3.JPG"
+    image_path = "example_images/example2.png"
 
     # languages: rus, eng, rus+eng
-    # text = tesseract_extract(image_path=image_path, lang="eng")
+    text = tesseract_extract(image_path=image_path, lang="rus")
+    with open("tesseract_output.txt", "w", encoding="utf-8") as f:
+        f.write(text)
 
     # languages: https://github.com/JaidedAI/EasyOCR/tree/master/easyocr/character
-    text = easy_extract(image_path=image_path, lang="en")
+    text = easy_extract(image_path=image_path, lang="ru")
+    with open("easy_output.txt", "w", encoding="utf-8") as f:
+        f.write(text)
 
     print(text)
 
